cbde45da80eb720f2ab50a98b442ab142fea7477
JamilJuez <jamiljuezlol@gmail.com>
2025-02-22 01:44:01 -0500

    pwabuilder

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,44 +1,47 @@
-const CACHE_NAME = "pwa-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/logo.png", // Asegúrate de que esté en la carpeta public
-  "/manifest.json",
-  "/index.css", // También cachea los archivos CSS
-  "/assets/logo.png", // O cualquier otra imagen que estés usando
-];
+// This is the "Offline page" service worker
 
-// Instalación del Service Worker
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache); // Cacheo de los archivos estáticos
-    })
-  );
-  self.skipWaiting(); // Fuerza la activación del Service Worker
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// Interceptar las solicitudes y servir desde caché si no hay internet
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request); // Si el archivo no está en caché, se obtiene de la red
-    })
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-// Activación del Service Worker para limpiar caches antiguos
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache); // Elimina las versiones antiguas del cache
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim(); // Asegura que el Service Worker tome el control de inmediato
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
