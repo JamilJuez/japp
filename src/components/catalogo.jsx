8448975d371ed01bcfe3e2a2c0ff3189007c5edc
JamilJuez <jamiljuezlol@gmail.com>
2025-11-22 13:06:14 -0500

    inventory update

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -75,7 +75,7 @@ const Catalogo = () => {
       <div className="franja-superior">
         <div className="beta-sello">
           <span className="beta-v1">BETA V1</span>
-          <span className="desarrollo">Inventory updated 11/16</span>
+          <span className="desarrollo">Inventory updated 11/21</span>
         </div>
 
         <div className="categoria-filtro-wrapper">
